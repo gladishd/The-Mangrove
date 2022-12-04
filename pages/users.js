2b36c905eb0de5f4335c6be22be442455fdcc206
gladishd <gladish.dean@gmail.com>
2022-12-03 20:21:07 -0600

    moves the styles.js from ./pages to ./helpers

--- pages/users.js
+++ pages/users.js
@@ -4,7 +4,7 @@ import FileBase from 'react-file-base64';
 import { useDispatch, useSelector } from 'react-redux';
 
 
-import useStyles from './styles';
+import useStyles from '../helpers/styles';
 import { createPost, updatePost } from '../helpers/actions/posts';
 
 /**
