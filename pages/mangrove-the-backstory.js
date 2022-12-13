b1431ed3c55827203e129a9af2bcb351311c3057
gladishd <gladish.dean@gmail.com>
2022-12-12 22:34:40 -0600

    One-time email code such as Bloomberg? Sent in JSON files of all the Ohio and Texas county elected officials if you could add that in that'd be great How? Do the exact same thing you did with Illinois and Pennsylvania. Where do the JSON files OhioCountyEO.json * WashingtonCountyEO.json and TexasCountyEO.json "go"? They go to like, something similar to public > data > States > Illinois > County > IllinoisCountyEO.json... therefore this is where the abstraction starts to break down: Inserts the files into their "corresponding" state locations like public > data > States > Washington > County > WashingtonCountyEO.json...

--- pages/mangrove-the-backstory.js
+++ pages/mangrove-the-backstory.js
@@ -1,29 +1,112 @@
-import { useEffect, useRef } from 'react';
+// import { useEffect, useRef } from 'react';
 
-export default function HomePage() {
+// export default function HomePage() {
 
-  const viewer = useRef(null);
+//   const viewer = useRef(null);
 
-  useEffect(() => {
-    import('@pdftron/webviewer').then(() => {
-      WebViewer(
-        {
-          path: '/lib',
-          initialDoc: '/files/mangroveBackstory.pdf',
-        },
-        viewer.current,
-      ).then((instance) => {
-        const { docViewer } = instance;
-        // you can now call WebViewer APIs here...
-      });
-    })
-  }, []);
+//   useEffect(() => {
+//     import('@pdftron/webviewer').then(() => {
+//       WebViewer(
+//         {
+//           path: '/lib',
+//           initialDoc: '/files/mangroveBackstory.pdf',
+//         },
+//         viewer.current,
+//       ).then((instance) => {
+//         const { docViewer } = instance;
+//         // you can now call WebViewer APIs here...
+//       });
+//     })
+//   }, []);
 
+
+//   return (
+//     <div>
+//       <div ref={viewer} style={{ height: "100vh" }}></div>
+//     </div>
+//   );
+
+// }
+
+// //https://github.com/The-Shy7/react-pdf-viewer/blob/main/src/App.js
+// //npm install @phuocng/react-pdf-viewer
+// import React from 'react';
+// import Viewer, { Worker } from '@phuocng/react-pdf-viewer';
+// import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
+// // import filePDF from '../public/files/mangroveBackstory.pdf'
+// // import './App.css'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
+//         <div id="pdfviewer">
+//           <Viewer fileUrl={"./mangroveBackstory.pdf"} />
+//         </div>
+//       </Worker>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React, { useEffect } from 'react'
+// import ReactGA from 'react-ga'
+// import PDFViewer from 'pdf-viewer-reactjs'
+
+// import Router from 'next/router'
+
+// const ExamplePDFViewer = () => {
+//   useEffect(() => {
+//     ReactGA.initialize('UA-161923401-1');
+//     ReactGA.pageview(window.location.pathname);
+//     const isWindowClient = typeof window === "object";
+//     const windowSize =
+//       isWindowClient ? window.innerWidth : null
+//   })
+//   return (
+//     <div style={{
+//       zoom: "70%"
+//     }}>
+//       <PDFViewer style={{
+//         zoom: "70%"
+//       }}
+//         document={{
+//           url: 'mangroveBackstory.pdf',
+//         }}
+//       />
+//     </div>
+//   )
+// }
+
+// export default ExamplePDFViewer
+
+import pdf from '../public/mangroveBackstory.pdf';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
+function MyApp() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
   return (
     <div>
-      <div ref={viewer} style={{ height: "100vh" }}></div>
+      <Document
+        // file='../public/mangroveBackstory.pdf'
+        file={pdf}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
     </div>
   );
-
 }
+
+export default MyApp;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
+// module.exports = {
+//   resolve: {
+//     extensions: ['.js', '.jsx']
+//   }
+// };
+// /**
+//  * reactjs - Invalid configuration object. Webpack has been initialised using a configuration object that does not match the API schema - Stack Overflow
+// https://stackoverflow.com/questions/42060243/invalid-configuration-object-webpack-has-been-initialised-using-a-configuration
+//  */
+
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
-  }
-};
-/**
- * reactjs - Invalid configuration object. Webpack has been initialised using a configuration object that does not match the API schema - Stack Overflow
-https://stackoverflow.com/questions/42060243/invalid-configuration-object-webpack-has-been-initialised-using-a-configuration
- */
+  },
+  // debug: true,
+  // entry: ['./sdocs.js'],
+  // output: {
+  //   filename: './[name].bundle.js'
+  // },
+  // module: {
+  //   loaders: [
+  //     { test: /\.css$/, loader: "style-loader!css-loader" },
+  //   ],
+  // },
+  // rules: [
+  //   { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+  // ]
+}
+//https://stackoverflow.com/questions/37934147/you-may-need-an-appropriate-loader-to-handle-this-file-type-with-webpack-and-c
+
