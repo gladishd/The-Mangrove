4fd3f0c280f1db06e5579411996a69eca57721bf
gladishd <gladish.dean@gmail.com>
2022-12-18 22:38:22 -0500

    Just invited Chase to join our "web scraping" group...apparently it was a good thing to remind Chase to join in if she is interested in developing The Mangrove, where politicians go to lay down. The thing about independently collecting all this data, "there's got to be a better way". We should also have a TODO bulletin. The idea is that instead of calling dispatch directly as they do in Redux with React Hooks, we can "use" something called Redux thunks. That means that we map the thunks' "dispatch" to props, and then call those native functions known as if (this.state.currentId) {       dispatch(updatePost(this.state.currentId, this.state.postData));     } else {       dispatch(createPost(this.state.postData));     } That is, we are looking for the updatePost and createPost functions that are to be thunks. How do these work on the original repository, https://github.com/thellllvirtuoso/project_mern_memories ? The updatePost function. What does it entail? In the original memories_project, we export the const updatePost thunk, that takes an id, post and sends it to dispatch via Redux. That is, it awaits the command api.updatePost(id, post); and then dispatches an update called ({ type: 'UPDATE', payload: data }); on this.state. Additionally, we take the export const updatePost on another page. Therefore, the first one is to update the post on the api object. This is a classic thunk. The second thing is to export const updatePost, that means invoke the axios.patch command which is native to the axios library. That is the second page. That is, the client/src/actions/posts.js contains the export const updatePost thunk, and the second client/src/api/index.js contains the "Second Export" that is, takes the post and sends it to axios. The rest of the thunk usage is just imports, import { createPost, updatePost } from '../../actions/posts';, and we also have controllers for Mongoose! The imports are third, and the Controllers for Mongoose are in fourth. Finally, we import the Controller updatePost into the server/routes/posts.js, which just means that we're able to use these four controllers: getPosts, createPost, updatePost, deletePost, etc. we are able to import them into the server/routes/posts.js folder and then invoke all of them upon the Router. Therefore, we have all of these flows. We "need" to define Thunks, axios functions, and mongoose controllers.

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,28 @@ import '../styles.css'
 import { CookiesProvider } from "react-cookie"
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
+// export default function MyApp({ Component, pageProps }) {
+//   console.log("gets invoked")
+//   return (<CookiesProvider><Component {...pageProps} /></CookiesProvider>)
+// }
+
+import thunk from 'redux-thunk';
+import reducers from '../social/client/src/reducers';
+import { legacy_createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)));
 export default function MyApp({ Component, pageProps }) {
   console.log("gets invoked")
-  return (<CookiesProvider><Component {...pageProps} /></CookiesProvider>)
+  return (
+    // <CookiesProvider>
+    //   <Component {...pageProps} />
+    //   </CookiesProvider>
+    <Provider store={store}>
+      <CookiesProvider>
+        <Component {...pageProps} />
+      </CookiesProvider>
+    </Provider>
+  )
 }
 
 // import React from 'react';
