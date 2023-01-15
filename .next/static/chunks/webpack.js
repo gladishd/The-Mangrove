41db6aec236904d68e8be2924bfeca546924b156
gladishd <gladish.dean@gmail.com>
2023-01-15 15:03:56 -0600

    https://stackoverflow.com/questions/64573177/unable-to-resolve-dependency-tree-error-when-installing-npm-packages First you should start to read the problem from the bottom to the top. Here @agm/core@3.0.0-beta.0 requires angular common 9.1.0 or 10.0.0. And the top message says that the angular common found is actually 11.0.3. And for us the react-bootstrap@"^2.5.0 is from the root project. That is the react-bootstrap actually found. Here react-bootstrap requires react@"^16.8.0 || ^17.0.0-rc.1 || ^18.0.0 from @react-aria/ssr@3.4.0. And the top message says that the react version actually found is react@"^18". What would happen if we modified the React Version? peer react@"^16.8.0 || ^17.0.0-rc.1 || ^18.0.0" from @react-aria/ssr@3.4.0 peer react@"^16.8.0 || ^17.0.0" from @material-ui/core@4.12.4 peer react@"^16.8.0 || ^17.0.0" from @material-ui/core@4.12.4 peer @material-ui/core@"^4.0.0" from @material-ui/icons@4.11.3 peer @material-ui/core@"^4.0.0" from @material-ui/icons@4.11.3 "Changes" "react": "^18", "@material-ui/core": "^4.12.4", to "@material-ui/core": "^4.0.0", "react": "^16.8.0  ^17.0.0-rc.1  ^18.0.0", in "dependencies": { of package.json. Also, "devDependencies": {     "@material-ui/core": "^4.12.4", "devDependencies": {     "@material-ui/core": "^4.0.0", npm install react@"^16.8.0 || ^17.0.0-rc.1 || ^18.0.0" How to install a specific version of an npm package - Nathan Sebhastian https://sebhastian.com/npm-install-specific-version/ npm ERR! See /Users/deangladish/.npm/eresolve-report.txt for a full report. On Vercel.app (instead of npm install --force npm install --legacy-peer-deps).

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd8288dc210ffa6a"; }
+/******/ 		__webpack_require__.h = function() { return "e9622e5f154543f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.8a1ed5c35c52e743.hot-update.js
+++ .next/static/webpack/webpack.8a1ed5c35c52e743.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "bd8288dc210ffa6a"; }
+/******/ 	__webpack_require__.h = function() { return "8c89a8d03ebaa7b9"; }
 /******/ }();
 /******/ 
 /******/ }
