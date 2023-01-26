559d0e991868d38ed4ff98ce870e24f14db1dd7a
gladishd <gladish.dean@gmail.com>
2023-01-26 17:50:42 -0600

    https://cloud.mongodb.com/v2/6310f50334d2812a3eb177c6#/metrics/replicaSet/6310f63d0fc49052e0e2cd0e/explorer/dev/users/find 🤙 it's amazing! The profile creation stuff: how the cookies are being set. Warning: Failed prop type: The prop `cookies` is marked as required in `Home`, but its value is `undefined`. withouspaces&registercommas@a.com password GET http://localhost:5000/auth/loggedIn POST http://localhost:5000/auth/ "errorMessage": "An account with this email already exists." Insomnia is the best 💨 POST http://localhost:5000/auth/login axios.post("http://localhost:5000/auth/login", loginData); loginData = {         email: this.state.email, password: this.state.password,       }; token    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjNjZjAyYjFlY2E0OTk2NTA5YWFjMWRhIiwiaWF0IjoxNjc0NzA4MTYwfQ.cbCe7LABMpORiVbejhDvTfLgluZMeSFjwmue-vmEaj0 {     "email": "withouspaces&registercommas@a.com",     "password": "password" } Delete the cookies and get false. So when you want to add a new thing to the user schema in userModel.js! JSON {     "email": "withouspaces&registercommas@a.com",     "password": "password" } POST http://localhost:5000/auth/ POST http://localhost:5000/auth/login GET http://localhost:5000/auth/loggedIn The true or false is predicated on the existence of the token cookie. 😓 🗯️ 😆 😢

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a25c3edd66baeed8"; }
+/******/ 		__webpack_require__.h = function() { return "6bc02a5920fdf7de"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c7eb780f7cc95f1d.hot-update.js
+++ .next/static/webpack/webpack.c7eb780f7cc95f1d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a25c3edd66baeed8"; }
+/******/ 	__webpack_require__.h = function() { return "6bc02a5920fdf7de"; }
 /******/ }();
 /******/ 
 /******/ }
