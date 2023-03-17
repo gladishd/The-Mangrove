9b479a430da78b79b8e1024394efad18101c7c6a
gladishd <gladish.dean@gmail.com>
2023-03-16 22:30:21 -0500

    Shows Financial Entities, Key Indicators and Financial Information, for each and all given articles.

---
+++ README.md
@@ -0,0 +1,3 @@
+python3 beautiful-soup.py
+pip3 install spacy
+python3 -m spacy download en_core_web_sm
--- beautiful-soup.py
+++ beautiful-soup.py
@@ -1,8 +1,73 @@
-from bs4 import BeautifulSoup
+import re
 import urllib.request
-html_doc = urllib.request.urlopen(
-    "https://apnews.com/article/ohio-train-derailment-updates-bf5a60de2243cd6f7f730096aa33294c")
-soup = BeautifulSoup(html_doc, 'html.parser')
-for link in soup.find_all('a'):
-    # It helps to find all anchor tags
-    print(link.get('href'))
+import spacy
+import requests
+from bs4 import BeautifulSoup
+
+# Send a request to the AP News homepage
+url = "https://apnews.com/"
+response = requests.get(url)
+
+# If the request is successful, parse the content
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Find all the anchor tags with the 'href' attribute
+    all_links = soup.find_all('a', href=True)
+
+    # Iterate through the links, filter and print the URLs of just the '/article' pages
+    article_links = []
+    for link in all_links:
+        href = link['href']
+        if href.startswith('/article/'):
+            article_url = url[:-1] + href
+            if article_url not in article_links:
+                article_links.append(article_url)
+
+    print("AP News /article page URLs:")
+
+    nlp = spacy.load("en_core_web_sm")
+
+    for article_url in article_links:
+        print(article_url)
+
+        # Fetch the HTML doc for the current article
+        html_doc = urllib.request.urlopen(article_url)
+        soup = BeautifulSoup(html_doc, 'html.parser')
+
+        # Extract the content of the article
+        content = soup.find('div')
+        paragraphs = content.find_all('p')
+
+        article_text = ""
+        for paragraph in paragraphs:
+            article_text += paragraph.text + " "
+
+        # Process the article text with spaCy NLP
+        doc = nlp(article_text)
+
+        # Print the financial entities found in the article
+        print("\nFinancial Entities:")
+        for entity in doc.ents:
+            if entity.label_ in ["ORG", "MONEY", "PERCENT"]:
+                print(f"{entity.text} ({entity.label_})")
+
+        # Find key indicators and financial information about companies
+        key_indicators = ["revenue", "earnings", "EBITDA", "gross margin", "net income",
+                          "operating margin", "P/E ratio", "EPS", "dividend yield"]
+
+        financial_info = []
+
+        for indicator in key_indicators:
+            pattern = re.compile(f"{indicator}[^.]*", re.IGNORECASE)
+            matches = pattern.findall(article_text)
+            if matches:
+                financial_info.extend(matches)
+
+        print("\nKey Indicators and Financial Information:")
+        for info in financial_info:
+            print(info)
+
+        print("\n---\n")
+else:
+    print("Failed to fetch AP News homepage.")
